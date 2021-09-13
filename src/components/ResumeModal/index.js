1015a6ec79addc177c7a38ba2b3f643ab01a3bb5
Kateryna B <butsneva.ekaterina@gmail.com>
2021-09-12 19:46:33 -0500

    update resume

--- src/components/ResumeModal/index.js
+++ src/components/ResumeModal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import resume from '../../assets/resume.pdf';
+import resume from '../../assets/Kateryna_Butsnieva_Resume.pdf';
 import './style.css'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
